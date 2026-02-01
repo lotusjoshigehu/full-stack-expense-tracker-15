0c58015cae810688e1d8696ca21650b63fa546de
lotusjoshigehu <joshikamal626@gmail.com>
2026-02-01 13:08:37 +0530

    Refactor createOrder to use axios and environment variables

--- services/cashfreeservices.js
+++ services/cashfreeservices.js
@@ -1,35 +1,37 @@
-const fetch = require("node-fetch");
+const axios = require("axios");
 
-async function createOrder(email) {
+const CASHFREE_BASE_URL = "https://sandbox.cashfree.com/pg";
 
-    const orderId = "ORD_" + Date.now();
-    const customerId = email.replace(/[^a-zA-Z0-9_-]/g, "_");
-
-    const response = await fetch("https://sandbox.cashfree.com/pg/orders", {
-        method: "POST",
+exports.createOrder = async (userId, email) => {
+  try {
+    const response = await axios.post(
+      `${CASHFREE_BASE_URL}/orders`,
+      {
+        order_id: `order_${Date.now()}`,
+        order_amount: 499,
+        order_currency: "INR",
+        customer_details: {
+          customer_id: userId.toString(),
+          customer_email: email,
+          customer_phone: "9999999999"
+        }
+      },
+      {
         headers: {
-            "Content-Type": "application/json",
-            "x-client-id": "TEST430329ae80e0f32e41a393d78b923034",
-            "x-client-secret": "TESTaf195616268bd6202eeb3bf8dc458956e7192a85",
-            "x-api-version": "2022-09-01"
-        },
-        body: JSON.stringify({
-            order_id: orderId,
-            order_amount: 499,
-            order_currency: "INR",
-            customer_details: {
-                customer_id: customerId,
-                customer_email: email,
-                customer_phone: "9999999999"
-            },
-            order_meta: {
-                return_url: "http://localhost:3000/payment-success?order_id={order_id}"
-            }
-        })
-    });
+          "x-client-id": process.env.CASHFREE_APP_ID,
+          "x-client-secret": process.env.CASHFREE_SECRET_KEY,
+          "Content-Type": "application/json",
+          "x-api-version": "2023-08-01"
+        }
+      }
+    );
 
-    const data = await response.json();
-    return data;
-}
+    return {
+      payment_session_id: response.data.payment_session_id
+    };
 
-module.exports = { createOrder };
+  } catch (err) {
+    console.error("Cashfree API Error:", err.response?.data || err.message);
+    throw err;
+  }
+};
